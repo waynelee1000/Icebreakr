1526c3f316825ceffdfbdf1f5dc98380f2829dd0
Sandile Keswa <me@sandile.io>
2015-09-19 17:19:45 -0400

    init

---
+++ app/src/main/java/org/tudev/icebreakr/LoginActivity.java
@@ -0,0 +1,29 @@
+package org.tudev.icebreakr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Bind fields to XML views
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLoginButtonClicked();
+            }
+        });
+    }
+
+    private void onLoginButtonClicked() {
+
+    }
+}
