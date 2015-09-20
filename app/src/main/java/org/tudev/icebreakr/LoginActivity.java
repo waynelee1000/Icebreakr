7a0476571acfa9dcd97e0ebb80048c548fa5f031
Vuk Petrovic <vukpetrovic1@yahoo.com>
2015-09-19 21:40:27 -0400

    bullshit

--- app/src/main/java/org/tudev/icebreakr/LoginActivity.java
+++ app/src/main/java/org/tudev/icebreakr/LoginActivity.java
@@ -1,33 +1,53 @@
 package org.tudev.icebreakr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class LoginActivity extends AppCompatActivity {
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        // initialize facebook sdk
+    public void onCreate() {
         FacebookSdk.sdkInitialize(getApplicationContext());
-        // Bind fields to XML views
-        Button loginButton = (Button) findViewById(R.id.login_button);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        callbackManager = CallbackManager.Factory.create();
+
+        setContentView(R.layout.activity_login);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onClick(View v) {
-                onLoginButtonClicked();
+            public void onSuccess(LoginResult loginResult) {
+                System.out.println("Access token: " + loginResult.getAccessToken().getToken());
+            }
+
+            @Override
+            public void onCancel() {
+                System.out.println("Canceled");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                System.out.println("Failed");
             }
         });
     }
 
-    private void onLoginButtonClicked() {
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
